604738c7a42bee1dfcf2eccf48d6fff3eae04abe
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-11-16 10:11:09 -0200

    Gerando um component através do angular CLI pelo comando: ng generate component photos/photo-list

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { PhotoComponent } from "./photo/photo.component";
 import { HttpClientModule } from "@angular/common/http";
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
 
     //Private
-    declarations: [ PhotoComponent ],
+    declarations: [ PhotoComponent, PhotoListComponent ],
 
     //Public
     exports: [ PhotoComponent ],
