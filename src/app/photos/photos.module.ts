34f9a5f5538bb2e097be64c33d8dbf3ef8401048
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-11-16 16:24:51 -0200

    corrigindo o bug do framework para que respeite o limite de cols

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { PhotoComponent } from "./photo/photo.component";
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
 
@@ -12,7 +13,8 @@ import { PhotoFormComponent } from './photo-form/photo-form.component';
     declarations: [ 
         PhotoComponent, 
         PhotoListComponent, 
-        PhotoFormComponent 
+        PhotoFormComponent, 
+        PhotosComponent 
     ],
 
     //Public
