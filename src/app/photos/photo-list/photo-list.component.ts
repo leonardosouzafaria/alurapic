2d9b0eaf577ea1c8d0ab5b69bacaf1a8deb045ae
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-11-17 11:50:43 -0200

    Criado funcionalidade de paginacao

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -12,27 +13,44 @@ import { Photo } from '../photo/photo';
 })
 
 export class PhotoListComponent implements OnInit, OnDestroy {
-  
+
   photos: Photo[] = [];
-  
+
   filter: string = '';
 
-  debounce : Subject<string> = new Subject<string>();
+  debounce: Subject<string> = new Subject<string>();
+
+  hasMore: boolean = true;
+
+  currentPage: number = 1;
+
+  userName: string = '';
 
   //Constructor ficará responsavel somente por injeções 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private photoService: PhotoService) { }
 
   //Fluxo de vída do componente
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
+
     this.photos = this.activatedRoute.snapshot.data.photos;
 
     this.debounce
       .pipe(debounceTime(300))
       .subscribe(filter => this.filter = filter);
   }
-  
+
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
   }
-  
+
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if (!photos.length) this.hasMore = false;
+      })
+  }
+
 }
