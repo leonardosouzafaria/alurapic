ddaca92eb41f6d3854b5ca89186b0dccd46186e6
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-11-17 15:26:19 -0200

    Criando a primeira diretiva

---
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { DarkenOnHoverDirective } from './darken-on-hover.directive';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [ DarkenOnHoverDirective ],
+    imports: [ CommonModule ]
+})
+export class DarkenOnHoverModule { }
