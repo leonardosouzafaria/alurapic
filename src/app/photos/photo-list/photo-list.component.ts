28aae2080fba50f2dcb8f5b6c705f9e7c50c80c2
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-11-17 10:36:50 -0200

    Inserindo um debounce no filtro de 300 ms

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 
@@ -15,11 +17,17 @@ export class PhotoListComponent implements OnInit {
 
   filter: string = '';
 
+  debounce : Subject<string> = new Subject<string>();
+
   //Constructor ficará responsavel somente por injeções 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   //Fluxo de vída do componente
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data.photos;
+
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
   }
 }
